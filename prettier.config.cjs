a8459c2fd7eebe827496e828ef9f04764b4544f9
Devon McGrath <devon.mcgrath@gmail.com>
2023-09-05 09:44:14 +1200

    Add prettier config file

---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/** @type {import("prettier").Config} */
+const config = {
+	singleQuote: true,
+	jsxSingleQuote: true,
+	tabWidth: 2,
+	useTabs: true,
+	semi: false,
+	printWidth: 100,
+	trailingComma: 'es5',
+}
+
+module.exports = config
